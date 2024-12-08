6c3fd34aef989ab01350652cc0b0e89e1d76a4d6
Ramasarvareddy <2200030527@kluniversity.in>
2024-12-08 05:32:01 +0530

    initial commit

---
+++ src/main/java/com/klu/FundSmart/FundSmartApplication.java
@@ -0,0 +1,13 @@
+package com.klu.FundSmart;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FundSmartApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FundSmartApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/klu/FundSmart/FundSmartApplicationTests.java
@@ -0,0 +1,13 @@
+package com.klu.FundSmart;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FundSmartApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
